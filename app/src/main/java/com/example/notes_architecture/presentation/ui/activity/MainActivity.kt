acb03d6c8a1c2a1f7643e7d3d8b2c0e51ed879e3
user <ayanakim258@gmail.com>
2023-04-26 00:16:20 +0600

    added kotlin-dsl

--- app/src/main/java/com/example/notes_architecture/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/notes_architecture/presentation/ui/activity/MainActivity.kt
@@ -10,7 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // added kotlin-dsl
-        // added app architecture
+
     }
 }
